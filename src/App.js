cf724b2c721d1f5559359d959714c6c1924a2825
Mike Belyayev <4xgood@gmail.com>
2024-08-29 20:28:02 -0600

    updated cicd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./store"; // Adjust the path to your store
 import Listings from "./Listings";
 
 function App() {
   return (
-    <div className="App">
-      <Listings />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Listings />
+      </div>
+    </Provider>
   );
 }
 
