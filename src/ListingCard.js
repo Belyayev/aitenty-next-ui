82187ec4d9a50578c783024f46920998bca568c2
Mike Belyayev <4xgood@gmail.com>
2024-08-29 21:23:42 -0600

    styled

--- src/ListingCard.js
+++ src/ListingCard.js
@@ -1,20 +1,25 @@
 import React from "react";
+import "./ListingCard.css";
 
 const ListingCard = ({ listing }) => {
   return (
     <div className="card">
-      <h2>{listing.Description}</h2>
-      <p>Price: ${listing.ListPrice}</p>
+      <h2 className="address">
+        {listing.Address_StreetNumber} {listing.Address_StreetName}{" "}
+        {listing.Address_StreetSuffix}, {listing.Address_City},{" "}
+        {listing.Address_StateOrProvince} {listing.Address_PostalCode}
+      </h2>
+      <p className="price">${listing.ListPrice}</p>
+      <p className="description">{listing.Description}</p>
       <p>Type: {listing.Type}</p>
       <p>SubType: {listing.SubType}</p>
       <p>Bedrooms: {listing.BedroomCount}</p>
       <p>Bathrooms: {listing.BathroomCount}</p>
-      <p>
-        Address: {listing.Address_StreetNumber} {listing.Address_StreetName}{" "}
-        {listing.Address_StreetSuffix}, {listing.Address_City},{" "}
-        {listing.Address_StateOrProvince} {listing.Address_PostalCode}
-      </p>
-      {/* Add more fields as needed */}
+      <p>Year Built: {listing.YearBuilt}</p>
+      <p>Parking: {listing.ParkingFeatures}</p>
+      <p>Community Features: {listing.CommunityFeatures}</p>
+      <p>Listing Agent: {listing.ListingAgent_Name}</p>
+      <p>Contact: {listing.ListingAgent_Email}</p>
     </div>
   );
 };
